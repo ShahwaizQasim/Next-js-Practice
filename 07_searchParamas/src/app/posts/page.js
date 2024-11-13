857b622e10b293258a71586af7e72fac6aafc604
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-13 03:33:55 -0800

    search params topic cover

---
+++ 07_searchParamas/src/app/posts/page.js
@@ -0,0 +1,30 @@
+import SearchInput from "@/components/searchInput";
+
+export default async function Posts({ searchParams }) {
+    console.log('searchParams', searchParams);
+    const { q, limit=100, skip = 0 } = searchParams;
+    let res = await fetch(q ?
+        `https://dummyjson.com/posts/search?q=${q}&limit=${limit}&skip=${skip}` :
+        `https://dummyjson.com/posts?limit=${limit}&skip=${skip}`);
+    res = await res.json();
+
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-center pt-10 text-3xl font-semibold">Posts</h1>
+            <SearchInput />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+
+                {
+                    res.posts.map((posts) => <div className="shadow rounded p-4 border">
+                        <h1 className="font-bold text-center">{posts?.title}</h1>
+                        <h2 className="text-center">{posts?.body.slice(0, 150)}</h2>
+                        <div className="flex justify-center gap-4">
+                            <h3 className="font-bold">Likes {posts?.reactions.likes}</h3>
+                            <h3 className="font-bold">Views {posts?.views}</h3>
+                        </div>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+}
---
+++ 07_searchParamas/src/components/searchInput.js
@@ -0,0 +1,28 @@
+'use client'
+
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+export default function SearchInput() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+    function handleSearch(search) {
+        const params = new URLSearchParams(searchParams);
+        console.log(search);
+        if (search) {
+            params.set('q', search);
+            replace(`${pathname}?${params.toString()}`);
+        } else {
+            params.delete('q');
+            replace(`${pathname}?${params.toString()}`);
+        }
+    }
+    return (
+        <input className="border p-2 w-full mt-5 mb-3" placeholder="search"
+            onChange={(e) => {
+                handleSearch(e.target.value);
+
+            }}
+            defaultValue={searchParams.get('q')?.toString()} />
+    )
+}
