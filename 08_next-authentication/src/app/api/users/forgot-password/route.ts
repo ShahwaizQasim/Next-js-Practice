df00353883ee120d23b437e6a278f1d45d2ffc3a
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-03-10 16:52:55 +0500

    forgot passwaord api complete

--- 08_next-authentication/src/app/api/users/forgot-password/route.ts
+++ 08_next-authentication/src/app/api/users/forgot-password/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from "@/helpers/mailer";
 import { UserModel } from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -8,7 +9,9 @@ export default async function POST(request: NextRequest) {
         if (!user) {
             return NextResponse.json({ error: true, msg: "User Not Found" }, { status: 400 })
         }
+        await sendEmail({ email, emailType: "RESET", userId: user._id });
+        return NextResponse.json({ error: false, msg: "Email Verified Successfully" }, { status: 200 })
     } catch (error) {
-
+        return NextResponse.json({ error: true, msg: (error as Error).message }, { status: 400 })
     }
 }
