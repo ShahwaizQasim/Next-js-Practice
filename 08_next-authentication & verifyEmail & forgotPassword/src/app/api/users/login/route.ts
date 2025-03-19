db9d6975a3bbaedb2741f38079041a05ffa68551
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-03-19 16:12:02 +0500

    next-auth done

--- 08_next-authentication & verifyEmail & forgotPassword/src/app/api/users/login/route.ts
+++ 08_next-authentication & verifyEmail & forgotPassword/src/app/api/users/login/route.ts
@@ -5,41 +5,52 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 export async function POST(req: NextRequest) {
-    try {
-        await dbConnect(); //connect db
-        const { email, password } = await req.json(); // user request
-        const user = await UserModel.findOne({ email });
-        if (!user) {
-            return NextResponse.json({ error: true, msg: "user not found", status: 404 });
-        }
-
-        // Check if email is verified
-        if (!user.isVerified) {
-            return NextResponse.json({ error: true, msg: "Please verify your email to log in" }, { status: 400 });
-        }
-
-        // Compare Password
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatch) {
-            return NextResponse.json({ error: true, msg: "your password is incorrect" }, { status: 401 });
-        }
-
-        // Generate JWT Token 
-        const token = jwt.sign({ userId: user._id, email: user.email }, process.env.JWT_SECRET_KEY! as string, { expiresIn: '1h' });
-
-        const response = NextResponse.json({
-            error: false,
-            msg: "login successfully",
-            user: user,
-            token,
-        })
-        response.cookies.set("token", token, {
-            httpOnly: true,
-        })
-        return response;
-
-    } catch (error) {
-        return NextResponse.json({ error: true, msg: (error as Error).message });
+  try {
+    await dbConnect(); //connect db
+    const { email, password } = await req.json(); // user request
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: true, msg: "user not found" },
+        { status: 404 }
+      );
     }
-}
 
+    // Check if email is verified
+    if (!user.isVerified) {
+      return NextResponse.json(
+        { error: true, msg: "Please verify your email to log in" },
+        { status: 400 }
+      );
+    }
+
+    // Compare Password
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return NextResponse.json(
+        { error: true, msg: "your password is incorrect" },
+        { status: 401 }
+      );
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET_KEY! as string,
+      { expiresIn: "1h" }
+    );
+
+    const response = NextResponse.json({
+      error: false,
+      msg: "login successfully",
+      user: user,
+      token,
+    });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+    return response;
+  } catch (error) {
+    return NextResponse.json({ error: true, msg: (error as Error).message });
+  }
+}
--- 09_next-auth/src/middleware.ts
+++ 09_next-auth/src/middleware.ts
@@ -1,22 +1,27 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  console.log("middleware file is running");
-  const isPublic =
-    path === "/login" || path === "/signup" || path === "/verifyemail";
-  const token = request.cookies.get("token")?.value || "";
-  if (isPublic && token) {
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET! || "12345",
+  });
+  const url = request.nextUrl;
+
+  if (
+    (token && url.pathname.startsWith("sign-in")) ||
+    url.pathname.startsWith("signup")
+  ) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  if (!isPublic && !token) {
+  if (!token && url.pathname.startsWith("/")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/login", "/signup"],
+  matcher: ["/", "/sign-in", "/signup"],
 };
